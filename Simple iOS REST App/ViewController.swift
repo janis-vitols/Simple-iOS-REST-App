9a62cb536f4ed9e68ab7a513e0ceb18870a3e206
Jānis Vītols <janis.vitols@icloud.com>
2015-09-21 19:04:09 +0300

    Initial Commit

---
+++ Simple iOS REST App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Simple iOS REST App
+//
+//  Created by Vitols Janis on 21/09/15.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
