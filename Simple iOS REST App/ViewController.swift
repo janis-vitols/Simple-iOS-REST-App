4fd232a0cd704b9ef4d4c8488fdcd9d361db6d4c
Jānis Vītols <janis.vitols@icloud.com>
2015-09-21 22:08:54 +0300

    Implement login functionality
    - Try to connect to local server on port :9999 with POST path `login`;
    - Returned status code is counted as:
      * 200 - successful login;
      * 400 - unsuccessful login.

--- Simple iOS REST App/ViewController.swift
+++ Simple iOS REST App/ViewController.swift
@@ -24,5 +24,30 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func loginClicked(sender: AnyObject) {
+        let myUrl = NSURL(string: "http://127.0.0.1:9999/login");
+        let login    = loginField.text
+        let password = passwordField.text
+        let postString = "login=\(login)&password=\(password)";
+        let request = NSMutableURLRequest(URL:myUrl!);
+            request.HTTPMethod = "POST";
+            request.HTTPBody = postString.dataUsingEncoding(NSUTF8StringEncoding);
+        
+        let task = NSURLSession.sharedSession().dataTaskWithRequest(request) {
+            data, response, error in
+            
+            if error != nil {
+                println("error=\(error)")
+                return
+            }
+            
+            if let httpResponse = response as? NSHTTPURLResponse {
+                let message = httpResponse.statusCode == 200 ? "Success" : "Fail"
+                println(message)
+            } // if let httpResponse
+        } // let task
+        
+        task.resume()
+    }
 }
 
